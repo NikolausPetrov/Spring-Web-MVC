870bf9151031d4be080e0c21dbf8a6e834d5f1be
NikolausPetrov <me@zapad23.ru>
2024-12-03 23:15:02 +0300

    Small fix

--- src/main/java/ru/netology/exception/NotFoundException.java
+++ src/main/java/ru/netology/exception/NotFoundException.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
-
     public NotFoundException() {
     }
 
